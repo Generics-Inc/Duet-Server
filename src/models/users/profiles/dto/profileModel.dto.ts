da58cff6941acd6fd46e273c35cac32662dc0be0
LorexIQ <i@liq-mail.ru>
2024-06-07 16:44:24 +0300

    fix: bug with undefined groupId in guard

--- src/models/users/profiles/dto/profileModel.dto.ts
+++ src/models/users/profiles/dto/profileModel.dto.ts
@@ -9,9 +9,6 @@ export class ProfileModelDto extends ProfileMinimalDto {
     @ApiProperty({ description: 'ID активной группы', type: Number, required: false, default: 0 })
     groupId?: number;
 
-    @ApiProperty({ description: 'ID VK', type: Number, required: false, default: 284470002 })
-    vkId?: number;
-
     @ApiProperty({ description: 'Дата последнего изменения профиля', type: Date })
     updatedAt: Date;
 }
