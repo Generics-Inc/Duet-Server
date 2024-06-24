c84e4ee3562faa0b8f211656bb7f7fdbb2684c01
LorexIQ <i@liq-mail.ru>
2024-06-24 22:53:21 +0300

    fix: dto's length blocks

--- src/models/movies/dto/createMovieModel.dto.ts
+++ src/models/movies/dto/createMovieModel.dto.ts
@@ -17,9 +17,9 @@ import {ApiProperty} from "@nestjs/swagger";
 
 
 export class CreateMovieModelDto {
-    @ApiProperty({ description: 'Название', type: String })
+    @ApiProperty({ description: 'Название', type: String, minLength: 1, maxLength: 500 })
     @IsString()
-    @Length(0, 100)
+    @Length(1, 500)
     name: string;
 
     @ApiProperty({ description: 'Тип', enum: MovieType })
@@ -40,28 +40,28 @@ export class CreateMovieModelDto {
     @Max(5000)
     time?: number;
 
-    @ApiProperty({ description: 'Страна производитель', type: String, required: false })
+    @ApiProperty({ description: 'Страна производитель', type: String, required: false, minLength: 1, maxLength: 50 })
     @IsOptional()
     @IsString()
     @Length(1, 50)
     country?: string;
 
-    @ApiProperty({ description: 'Оригинальное название', type: String, required: false })
+    @ApiProperty({ description: 'Оригинальное название', type: String, required: false, minLength: 1, maxLength: 500 })
     @IsOptional()
     @IsString()
-    @Length(0, 100)
+    @Length(1, 500)
     originalName?: string;
 
-    @ApiProperty({ description: 'Слоган', type: String, required: false })
+    @ApiProperty({ description: 'Слоган', type: String, required: false, minLength: 1, maxLength: 1000 })
     @IsOptional()
     @IsString()
-    @Length(0, 1000)
+    @Length(1, 1000)
     slogan?: string;
 
-    @ApiProperty({ description: 'Описание', type: String, required: false })
+    @ApiProperty({ description: 'Описание', type: String, required: false, minLength: 1, maxLength: 5000 })
     @IsOptional()
     @IsString()
-    @Length(0, 5000)
+    @Length(1, 5000)
     description?: string;
 
     @ApiProperty({ description: 'Дата релиза', type: Date, required: false })
--- src/modules/movies/dto/createMovieAsync.dto.ts
+++ src/modules/movies/dto/createMovieAsync.dto.ts
@@ -9,14 +9,14 @@ export class CreateMovieAsyncDto {
     @Length(1, 200)
     link: string;
 
-    @ApiProperty({ description: 'Название', type: String, minLength: 1, maxLength: 100 })
+    @ApiProperty({ description: 'Название', type: String, minLength: 1, maxLength: 500 })
     @IsString()
-    @Length(1, 100)
+    @Length(1, 500)
     name: string;
 
-    @ApiProperty({ description: 'Дополнительное имя', type: String, minLength: 1, maxLength: 100 })
+    @ApiProperty({ description: 'Дополнительное имя', type: String, minLength: 1, maxLength: 500 })
     @IsString()
-    @Length(1, 100)
+    @Length(1, 500)
     addName: string;
 
     @ApiProperty({ description: 'Тип', enum: MovieType })
